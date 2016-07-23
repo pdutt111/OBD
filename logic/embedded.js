14fa7d105f3a25c7f0ccdc9e261374ba8db88cf8
pdutt111 <pdutt111@gmail.com>
2016-07-24 02:40:03 +0530

    distance first row error resolved

--- logic/embedded.js
+++ logic/embedded.js
@@ -25,44 +25,48 @@ require('net').createServer(function (socket) {
             }else{
                 var gprmc=nmea.parse("$"+data_array[1]);
             }
-            if(gprmc.valid){
+            if(gprmc.valid) {
                 log.info(gprmc);
-                data_object_location.location=gprmc.loc.geojson.coordinates;
-            locationTable.find({device_id:data_object_health.device_id},"location").sort({_id:-1}).limit(1)
-                .exec(function(err,row){
-                    data_object_location.distance=haversineDistance(row.location,data_object_location.location,false);
-            data_object_health.voltage=data_array[2];
-            if(data_array[3].slice(0,1)=="41"){
-                data_object_health.coolant_temp=(Number(data_array[3].slice(4,5))-40);//410582
-            }
-            if(data_array[4].slice(0,1)=="41"){
-                data_object_health.vehicle_speed_sensor=Number(data_array[4].slice(4,5));//410D01
-                data_object_location.speed=Number(data_array[4].slice(4,5));//410D01
-            }
-            if(data_array[5].slice(0,1)=="41")
-            data_object_health.engine_rpm=((256*Number(data_array[5].slice(4,5))+Number(data_array[5].slice(6,7)))/4);//410C13DC
-            if(data_array[6].slice(0,1)=="41")
-            data_object_health.intake_air_temp=(Number(data_array[6].slice(4,5))-40);//410F62
-            if(data_array[7].slice(0,1)=="41")
-            data_object_health.throttle_position=(Number(data_array[7].slice(4,5))*100/255);//41111A
-            if(data_array[8].slice(0,1)=="41")
-            data_object_health.run_time=(256*Number(data_array[8].slice(4,5))+Number(data_array[8].slice(6,7)));//7F0112
-            if(data_array[9].slice(0,1)=="41")
-            data_object_health.fuel_level=(Number(data_array[9].slice(4,5))*100/255);
-            if(data_array[10].slice(0,1)=="41")
-            data_object_health.ambient_air_temp=(Number(data_array[10].slice(4,5))-40);
-            if(data_array[11].slice(0,1)=="41")
-            data_object_health.engine_oil_temp=(Number(data_array[11].slice(4,5))-40);
-            if(data_array[12].slice(0,1)=="41")
-            data_object_health.engine_fuel_rate=((256*Number(data_array[5].slice(4,5))+Number(data_array[5].slice(6,7)))/120);
+                data_object_location.location = gprmc.loc.geojson.coordinates;
+                locationTable.find({device_id: data_object_health.device_id}, "location").sort({_id: -1}).limit(1)
+                    .exec(function (err, row) {
+                        if (row && row.location) {
+                            data_object_location.distance = haversineDistance(row.location, data_object_location.location, false);
+                        }else{
+                            data_object_location.distance = 0;
+                        }
+                            data_object_health.voltage = data_array[2];
+                            if (data_array[3].slice(0, 1) == "41") {
+                                data_object_health.coolant_temp = (Number(data_array[3].slice(4, 5)) - 40);//410582
+                            }
+                            if (data_array[4].slice(0, 1) == "41") {
+                                data_object_health.vehicle_speed_sensor = Number(data_array[4].slice(4, 5));//410D01
+                                data_object_location.speed = Number(data_array[4].slice(4, 5));//410D01
+                            }
+                            if (data_array[5].slice(0, 1) == "41")
+                                data_object_health.engine_rpm = ((256 * Number(data_array[5].slice(4, 5)) + Number(data_array[5].slice(6, 7))) / 4);//410C13DC
+                            if (data_array[6].slice(0, 1) == "41")
+                                data_object_health.intake_air_temp = (Number(data_array[6].slice(4, 5)) - 40);//410F62
+                            if (data_array[7].slice(0, 1) == "41")
+                                data_object_health.throttle_position = (Number(data_array[7].slice(4, 5)) * 100 / 255);//41111A
+                            if (data_array[8].slice(0, 1) == "41")
+                                data_object_health.run_time = (256 * Number(data_array[8].slice(4, 5)) + Number(data_array[8].slice(6, 7)));//7F0112
+                            if (data_array[9].slice(0, 1) == "41")
+                                data_object_health.fuel_level = (Number(data_array[9].slice(4, 5)) * 100 / 255);
+                            if (data_array[10].slice(0, 1) == "41")
+                                data_object_health.ambient_air_temp = (Number(data_array[10].slice(4, 5)) - 40);
+                            if (data_array[11].slice(0, 1) == "41")
+                                data_object_health.engine_oil_temp = (Number(data_array[11].slice(4, 5)) - 40);
+                            if (data_array[12].slice(0, 1) == "41")
+                                data_object_health.engine_fuel_rate = ((256 * Number(data_array[5].slice(4, 5)) + Number(data_array[5].slice(6, 7))) / 120);
 
-            var location=new locationTable(data_object_location);
-            location.save(function(err,row,info){
-            });
-            var health=new healthTable(data_object_health);
-            health.save(function(err,row,info){
-            });
-                });
+                            var location = new locationTable(data_object_location);
+                            location.save(function (err, row, info) {
+                            });
+                            var health = new healthTable(data_object_health);
+                            health.save(function (err, row, info) {
+                            });
+                    });
             }
         });
     })
